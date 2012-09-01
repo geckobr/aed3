a62f4bc60c92726602dcb13f398dd0b379f3ba1b
Felipe Prado <felipe@hp.(none)>
2012-09-01 02:03:07 -0300

    teste git

---
+++ main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: felipe
+ *
+ * Created on 28 de Agosto de 2012, 18:45
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
